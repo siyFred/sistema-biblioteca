b80b10403ea0543a11cf560755dbb709e8f935e9
siyFred <fred87gamer@gmail.com>
2025-12-03 01:33:54 -0300

    fix(books): correção no upload de arquivos (cover_image) e validação do PATCH no swagger

--- backend/books/views.py
+++ backend/books/views.py
@@ -29,16 +29,48 @@ BOOK_SCHEMA = {
     }
 }
 
+# Schema pra resolver bug do swagger
+BOOK_PATCH_SCHEMA = {
+    'multipart/form-data': {
+        'type': 'object',
+        'properties': BOOK_SCHEMA['multipart/form-data']['properties'],
+        'required': [] #
+    }
+}
+
 @extend_schema_view(
     create=extend_schema(description="Cria um novo livro", request=BOOK_SCHEMA),
     update=extend_schema(description="Atualiza um livro", request=BOOK_SCHEMA),
-    partial_update=extend_schema(description="Atualização parcial", request=BOOK_SCHEMA),
+    partial_update=extend_schema(description="Atualização parcial", request=BOOK_PATCH_SCHEMA),
 )
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all().order_by('-created_at')
     serializer_class = BookSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
     parser_classes = (MultiPartParser, FormParser)
+
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        data = request.data
+        
+        # resolve bug no swagger
+        if partial:
+            new_data = data.copy()
+            for key, value in data.items():
+                if value == '' and key not in ['cover_image', 'cover_url']:
+                     if key in new_data:
+                         new_data.pop(key)
+            data = new_data
+
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+
+        if getattr(instance, '_prefetched_objects_cache', None):
+            instance._prefetched_objects_cache = {}
+
+        return Response(serializer.data)
 
 
 class ImportBookView(views.APIView):
